8f31ef121e30f9433247394c5ab7b8496544b478
sujonbiswas01 <sujonbiswasdev@gmail.com>
2025-08-08 01:44:00 +0600

    javascript

---
+++ Loop.js
@@ -0,0 +1,26 @@
+// let num = 1;
+// while(num<=10){
+//     if(num<=10){
+//         const result = num*3;
+//         console.log(result)
+//     }
+//     num++
+    
+// }
+// let num = 1
+// while(num<=110){
+//     const num1 = num===30;
+//     if(num1){
+//         const result = 111/num;
+//         console.log(result)
+//     }
+//     num++
+// }
+
+// let num = 20;
+// let sum = 0;
+// while(num<=40){
+//     num+=1
+//     sum = sum+num
+// }
+// console.log("minimum number of 20 and maximum number of 40 create by sum : "+sum)
---
+++ arrow.js
@@ -0,0 +1,39 @@
+// // const arr = [10,20,30,40,50];
+// // arr.unshift(60)
+// // // const arr1 = arr[0]
+// // // const arr2 = arr[1]
+// // // const arr3 = arr[2]
+// // // console.log(arr1+arr2+arr3)
+// // console.log(arr)
+// // var sum = 0;
+// // for(var i = 0; i<arr.length;i++){
+// //     if(arr[i] <=30){
+// //        sum= sum+arr[i]
+// //     }
+// // }
+// // console.log(sum)
+
+// const arr = ["apple","banana","mango","lichu"];
+
+// const indexchack = arr.indexOf("mango")
+// console.log(indexchack)
+// if(arr.includes('mango')){
+//     console.log("ase")
+// }
+// else{
+//     console.log("nai")
+// }
+
+// const arr= ['digi','mag','bristi','borsha']
+// const check = arr.includes('bristi');
+// if(check){
+//     console.log('ase')
+// }
+// else{
+//     console.log('nai')
+// }
+
+// const arr = ['java','python','c','javascript','c#','c++']
+// for(const num of arr){
+//     console.log(num.trim(""))
+// }

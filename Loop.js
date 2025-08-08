637d1133e9c16262cb62fa13101c93b186907f5a
sujonbiswas01 <sujonbiswasdev@gmail.com>
2025-08-09 02:39:33 +0600

    problem solve

--- Loop.js
+++ Loop.js
@@ -24,3 +24,83 @@
 //     sum = sum+num
 // }
 // console.log("minimum number of 20 and maximum number of 40 create by sum : "+sum)
+
+// for(i=150;i<=170;i++){
+//     console.log(i)
+// }
+
+// let sum = 0;
+// for(i=3;i<=54;i++){
+//     sum = sum+i
+// }
+// console.log(sum)
+
+// let sum = 0;
+// for(i=25;i<=75;i++){
+//     sum = sum+i
+// }
+// console.log(sum)
+
+// for(i=20;i<=50;i++){
+//     if(i%7==0){
+//         console.log(i)
+//     }
+// }
+
+// for(i=40;i<=80;i++){
+//     if(i%5==0 || i%7==0){
+//         console.log(i)
+//     }
+// }
+// let sum = 0;
+// for(i=1;i<=40;i++){
+//     if(i%13==0){
+//         sum = sum + i
+//     }
+// }
+// console.log(sum)
+// for(i=1;i<=50;i=i+4){
+//     console.log(i)
+// }
+
+// for(i=0;i<100;i++){
+//     if(i%9==0||i%6==0){
+//         console.log(i)
+//     }
+// }
+
+// let sum = 0
+// for(i=1;i<=50;i++){
+//     if(i%3==0 || i%4==0){
+//         sum = sum+i
+//     }
+// }
+// console.log(sum)
+
+// for(i=1;i<=30;i++){
+//     console.log(i)
+//     if(i==15){
+//         break
+//     }
+// }
+// for(i=1;i<=30;i++){
+   
+//     if(i==15){
+//         continue
+//     }
+//      console.log(i)
+// }
+
+// for(i=1;i<=40;i++){
+//     if(i%7==0){
+//         continue
+//     }
+//     console.log(i)
+// }
+
+for(i=1;i<=15;i++){
+    if(i==9){
+        continue
+    }
+    console.log(i)
+}

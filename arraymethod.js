ffffd69a9b4b3214cea4dc4b0c093308d7966785
sujonbiswasdev <sujonbiswasdev@gmail.com>
2025-09-10 16:21:22 +0600

    s

---
+++ arraymethod.js
@@ -0,0 +1,28 @@
+// function sujon(number){
+//     const doubleArray = []
+//     for(num of number){
+//        const doublenumber = 2 * num
+//        doubleArray.push(doublenumber)                                                                                                            
+//     }
+//     return doubleArray
+// }
+// const array = [60,10,30,40,50]
+// console.log(sujon(array))
+
+
+// const array = [40,30,50,90,33]
+// const result = array.filter((num)=>num>32)
+// if(result){
+//     console.log("pass"+result)
+// }
+// else{
+//     console.log('fail'+ result)
+// }
+
+
+// tree paramiter recive
+// const array = [20,30,40,56]
+// const result = array.map((num,index,fullArray)=>{
+//     return `element : ${num} index : ${index} fullArray : ${fullArray}`
+// })
+// console.log(result)

6a38ee3b0527c9fd7a04418e89d36c9a766bfd64
sujonbiswasdev <sujonbiswasdev@gmail.com>
2025-09-23 17:46:01 +0600

    react call json

---
+++ react-for-json-call/vite-project/src/App.jsx
@@ -0,0 +1,39 @@
+
+import { useState } from 'react'
+import './App.css'
+import { useEffect } from 'react'
+
+function App() {
+  const [data,setdata]= useState([])
+  useEffect(()=>{
+    fetch('/data/complex1.json').then((res)=>{
+      return res.json()
+    }).then((res)=>setdata(res))
+  })
+  return (
+    <>
+     <h1>sujon biswas</h1>
+     {data.map((item,index)=>{
+      return <div key={index}>
+        <li>{item.name}</li>
+        <li>{item.email}</li>
+        <li>{item.address.city}</li>
+        <li>{item.address.country}</li>
+        <li>{item.title}</li>
+        <div>
+          {item.address.postalCode}
+        </div>
+        <div>
+          {item.wears.map((item,index)=>{
+            return <div key={index}>
+              {item}
+            </div>
+          })}
+        </div>
+      </div>
+     })}
+    </>
+  )
+}
+
+export default App
